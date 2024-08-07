b6a7c6b9699ea6f684d18f9dc4b0ec48fd8672e9
Shanushajahan707 <shanushajahan707@gmail.com>
2024-08-07 16:08:41 +0530

    after hosting

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
 export  const corsOptions = {
-    origin: 'http://localhost:4200', // Allow all origins
+    // origin: 'http://localhost:4200', // Allow all origins
+    origin: 'https://weathertouch-shanushajahan707s-projects.vercel.app', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true  
