a482ec95deba03e2ca8731d063ede6bcb5f0c1bd
Shanushajahan707 <shanushajahan707@gmail.com>
2025-04-14 11:33:44 +0530

    new things updated

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export  const corsOptions = {
     // origin: 'http://localhost:4200', // Allow all origins
-    origin: 'https://weathertouch-shanushajahan707s-projects.vercel.app', // Allow all origins
+    origin: 'https://weather-touch.vercel.app', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true  
